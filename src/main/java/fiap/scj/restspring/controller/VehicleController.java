52dd81399c39d51652b528a3c078ca89e921fb45
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-13 22:40:55 -0300

    aula rest spring

--- src/main/java/fiap/scj/restspring/controller/VehicleController.java
+++ src/main/java/fiap/scj/restspring/controller/VehicleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/vehicles")
 public class VehicleController {
 
-	@GetMapping("/")
+	@GetMapping("/teste")
 	public String olaMundo() {
 		return "teste";
 	}
